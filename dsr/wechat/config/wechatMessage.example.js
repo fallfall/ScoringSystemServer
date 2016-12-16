931807e1f4512424640b98462766ec17e036b2a0
nodejh <jianghangscu@gmail.com>
2016-12-17 00:03:14 +0800

    debug: 判断openid是否存在时输出parsed

---
+++ dsr/wechat/config/wechatMessage.example.js
@@ -0,0 +1,14 @@
+const menu = require('./menu');
+
+// 统一的回复消息
+const message = {
+  welcome: [{
+    title: 'xxx',
+    description: 'xxx',
+    picurl: 'xxx',
+    url: menu.news,
+  }],
+};
+
+
+module.exports = message;
